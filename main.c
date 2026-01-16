0594183e3824f2002c19e8566e761a99fc1ec76d
shubhojit-mitra-dev <Shubhojit.120225@stu.upes.ac.in>
2026-01-17 01:54:45 +0530

    add: basic typedefs for tokens and reading the ae file

--- main.c
+++ main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+  SEMI,
+  OPEN_PAREN,
+  CLOSE_PAREN,
+} TypeSeparator;
+
+typedef enum {
+  EXIT,
+} TypeKeyword;
+
+typedef enum {
+  INT,
+} TypeLiteral;
+
+typedef struct {
+  TypeKeyword type;
+} TokenKeyword;
+
+typedef struct {
+  TypeSeparator type;
+} TokenSeparator;
+
+typedef struct {
+  TypeLiteral type;
+  int value;
+} TokenLiteral;
+
 int main() {
-  printf("Hello World\n");
+  FILE *file;
+  file = fopen("test.ae", "r");
+  char curr;
+
+  while ((curr = fgetc(file)) != EOF) {
+    printf("%c", curr);
+  }
+
+  fclose(file);
   return 0;
 }
